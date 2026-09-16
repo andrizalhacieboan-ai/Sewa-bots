292e09962ba0315af940faec700a2dd34963f5d0
ANDRI-STORE <andrizalhacieboan@gmail.com>
2026-09-16 09:57:23 +0700

    Fix content array indentation in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
+    content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
